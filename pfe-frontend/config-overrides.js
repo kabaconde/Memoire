3c01be2947433a7a4c2b0ce04ec384a24b58d900
kabaconde2024 <kabaconde5259@gmail.com>
2026-06-08 20:55:23 +0100

    fix: add cache bust and correct proxy URLs

--- pfe-frontend/config-overrides.js
+++ pfe-frontend/config-overrides.js
@@ -1,12 +1,5 @@
 // config-overrides.js
-const path = require('path');
-
 module.exports = function override(config, env) {
-    // On cible désormais le fichier dans le dossier autorisé src/
-    config.resolve.alias = {
-        ...config.resolve.alias,
-        '@mui/material$': path.resolve(__dirname, 'src/mui-patch.js')
-    };
-
+    // Aucune modification d'alias ici, on laisse Webpack gérer MUI nativement
     return config;
 };
