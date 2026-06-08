07e7c422ade9e2fff0955ecc38db0c858644e99b
kabaconde2024 <kabaconde5259@gmail.com>
2026-06-08 20:31:18 +0100

    fix: add cache bust and correct proxy URLs

--- pfe-frontend/config-overrides.js
+++ pfe-frontend/config-overrides.js
@@ -1,20 +1,5 @@
 // config-overrides.js
-const webpack = require('webpack');
-
 module.exports = function override(config, env) {
-    // Ajouter un alias pour résoudre le problème CloseIcon
-    config.resolve.alias = {
-        ...config.resolve.alias,
-        '@mui/material/Close': '@mui/icons-material/Close',
-        '@mui/material/CloseIcon': '@mui/icons-material/Close'
-    };
-    
-    // Plugin pour injecter l'export manquant
-    config.plugins = (config.plugins || []).concat([
-        new webpack.ProvidePlugin({
-            CloseIcon: ['@mui/icons-material', 'Close']
-        })
-    ]);
-    
+    // Fichier nettoyé des alias conflictuels de CloseIcon
     return config;
 };
