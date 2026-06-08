734ccdadb43a01a6e9e9f56e47f5662ef974c9d2
kabaconde2024 <kabaconde5259@gmail.com>
2026-06-08 20:46:58 +0100

    fix: add cache bust and correct proxy URLs

--- pfe-frontend/config-overrides.js
+++ pfe-frontend/config-overrides.js
@@ -2,10 +2,10 @@
 const path = require('path');
 
 module.exports = function override(config, env) {
-    // On redirige l'import exact de @mui/material vers notre patch complet
+    // On cible désormais le fichier dans le dossier autorisé src/
     config.resolve.alias = {
         ...config.resolve.alias,
-        '@mui/material$': path.resolve(__dirname, 'mui-patch.js')
+        '@mui/material$': path.resolve(__dirname, 'src/mui-patch.js')
     };
 
     return config;
