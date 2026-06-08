de86426e28b496d43662d4469f9b0ac40c60b9ee
kabaconde2024 <kabaconde5259@gmail.com>
2026-06-08 20:51:09 +0100

    fix: add cache bust and correct proxy URLs

--- pfe-frontend/src/index.js
+++ pfe-frontend/src/index.js
@@ -1,19 +1,9 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-// --- PATCH DE SECOURS POUR LE BUILD RENDER ---
-// On importe l'objet global Material UI et l'icône Close officielle
-import * as MuiMaterial from '@mui/material';
-import CloseIcon from '@mui/icons-material/Close';
-
-// On injecte dynamiquement l'export manquant pour satisfaire la dépendance tierce
-if (MuiMaterial && !MuiMaterial.Close) {
-  MuiMaterial.Close = CloseIcon;
-}
-// ---------------------------------------------
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
