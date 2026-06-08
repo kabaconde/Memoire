af87abf1ce0ba900a689a8e072ce5b4b87ff8a48
kabaconde2024 <kabaconde5259@gmail.com>
2026-06-08 19:25:46 +0100

    fix: add cache bust and correct proxy URLs

--- pfe-frontend/src/index.js
+++ pfe-frontend/src/index.js
@@ -4,13 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// --- PATCH DE SECOURS POUR LE BUILD RENDER ---
+// On importe l'objet global Material UI et l'icône Close officielle
+import * as MuiMaterial from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+
+// On injecte dynamiquement l'export manquant pour satisfaire la dépendance tierce
+if (MuiMaterial && !MuiMaterial.Close) {
+  MuiMaterial.Close = CloseIcon;
+}
+// ---------------------------------------------
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
